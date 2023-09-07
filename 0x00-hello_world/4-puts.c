a06e45a21ac33a721d52cffc87be63b130058868
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-07 12:02:59 -0700

    Task4_Using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - here is the main function of the C file and function used
+ * Return: should always be 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
