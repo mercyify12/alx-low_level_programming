fdf368e8efafc74accca8876a812a896c2ade5b1
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-09 15:33:25 -0700

    Task2_using putchar funtion to prinf alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this function serves as the starting point for program execution
+ *
+ * Return: most times it is 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
