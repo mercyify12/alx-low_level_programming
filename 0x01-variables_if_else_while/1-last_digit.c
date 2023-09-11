b1070c3ae77e8a99c7e09223e39d6d7258eb3331
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-11 07:40:15 -0700

    Task1_finding the last digit of a variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-#include <stdio.h>
-/* betty style doc for function main goes there */
+
 /**
- * main - this function serves as the enrty point for program execution
- * Descrition: this task is to check last digits
- * Return: mostly end with 0
+ * main - this is the entry point where the programm is executed
+ * Description - to print last digit stored in a variable
+ * Return: mostly 0
  */
+
 int main(void)
 {
-	int n, m;
+	int n, lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	m = n % 10;
-	if (m > 5)
+	lastd = n % 10;
+
+	if (lastd > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, m);
+	printf("last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
-	else if (m < 6 && m != 0)
+	else if (lastd == 0)
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
-	else
+	else if (lastd < 6 && lastd != 0)
 	{
-		printf("last digit of %d is %d and is 0\n", n, m);
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
