a468792d59c122fb0b081efff923f4e7c72acef5
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-13 11:28:46 -0700

    Task10_Adding two intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - addition of two intergers
+ * @n2 - the first interger or number to be added
+ * @n3 - the second interger or number to be added
+ * Return: value of thr added interger
+ */
+
+int add(int n2, int n3)
+
+{
+	return (n2 + n3);
+}
