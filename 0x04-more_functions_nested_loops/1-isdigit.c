0a2a8dac4321e1acdf39789d8c5641cdf91a75d3
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-14 11:04:20 -0700

    Task1_functions that checks for digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,15 @@
-#include "main.c"
+#include "main.h"
 
 /**
- * _isdigit - 
- * @C: 
- * Return: 
+ * _isdigit - to check for a digit
+ * @c: Parameter to be checked
+ * Return: 1 for digit character or 0 for otherwise
  */
-
 int _isdigit(int c)
-
 {
-if (c
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
