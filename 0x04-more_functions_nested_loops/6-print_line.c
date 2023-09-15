c8dcf3f290dfdeecf1e1fa8cae2770de2f5238e3
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-14 19:42:51 -0700

    Task6_drawing a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - drawing straight line
+ * @n: number of lines
+ * Return: mostly 0
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
