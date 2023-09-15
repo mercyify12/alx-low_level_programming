1946088a1110325fac68ab3268959db195e50a0c
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-14 18:05:14 -0700

    Task2_multiples of two interger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiples of two integers
+ * @a: integer number 1
+ * @b: integer number 2
+ * Return: mostly 0
+ */
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
