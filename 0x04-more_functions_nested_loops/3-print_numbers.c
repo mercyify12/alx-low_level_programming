9e553f50acfc3a84f0c890fad3afa2ef0e2cb1c5
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-14 18:59:06 -0700

    Task3_print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers
+ * Return: mostly 0
+ */
+
+void print_numbers(void)
+
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
