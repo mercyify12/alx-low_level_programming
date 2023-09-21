81b1f7ed1e1f0d595f2a31cc89cc494af80c2d94
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-21 12:25:46 -0700

    Task7_encoding a sring of 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,18 +12,18 @@ char *leet(char *n)
 	int k;
 	int r;
 
-	char s1[] = "aAeEoOtTiL";
+	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
 	for (k = 0; n[k] != '\0'; k++)
 	{
-		for (r = 0; r < 10; r++)
-		{
-			if (n[k] == s1[r])
-			{
-				n[k] = s2[r];
-			}
-		}
+	for (r = 0; r < 10; r++)
+	{
+	if (n[k] == s1[r])
+	{
+	n[k] = s2[r];
+	}
+	}
 	}
 	return (n);
 }
