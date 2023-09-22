2ffe54e7578b9aea3038f29af87af27e867e648f
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-21 18:08:17 -0700

    Task9_ Printing Numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - main function that print integer
+ * @n: number of integer to be printed
+ * Return: mostly 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
