46878fb49a9d0e6bfd53998ba047efd71d072b90
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-26 17:42:51 -0700

    Task5_ strstr(function that locates a substring)

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - entry point of the main function that locates a substring
+ * @needle: function parameter that locates a substring
+ * @haystack: second parameter of the function
+ * Return: mostly 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *I = haystack;
+		char *P = needle;
+
+		while (*I == *P && *P != '\0')
+		{
+			I++;
+			P++;
+		}
+		if (*P == '\0')
+			return (haystack);
+	}
+	return (0);
+}
