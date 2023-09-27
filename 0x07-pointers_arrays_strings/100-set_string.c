232a4c94c2d3ef0c85628165bd076ce9366a62fd
mercyify12 <mmercyifeoma111@gmail.com>
2023-09-26 19:03:55 -0700

    Task8_sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - main function that sets the value of a pointer to a char
+ * @s: first functin of the parameter
+ * @to: second function of the parameter
+ * Return: mostly 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
