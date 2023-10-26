1db6acaf78e60d029d698a3f2ef952792b7e37e5
mercyify12 <mmercyifeoma111@gmail.com>
2023-10-25 21:59:32 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - function that adds a new node at the
+ * beginning of a linked list
+ * @head: pointing to the first node
+ * @n: data to insert in that new node
+ * Return: points to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new;
+
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+
+new->n = n;
+new->next = *head;
+*head = new;
+
+return (new);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+This is a readme file for the project 0x13-more_singly_linked_lists
+this task is very important in by explaining more about singly linkd list
